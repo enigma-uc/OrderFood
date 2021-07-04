97a9c66251d8c7dd863a0dac5539f49a4c80bdda
satyamjha <satyamjha@urbancompany.com>
2021-07-05 00:56:47 +0530

    Added Nav Bar

---
+++ app/src/main/java/com/example/orderfood/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.orderfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavBar: BottomNavigationView
+    private lateinit var frameContainer: FrameLayout
+    private lateinit var frag: Fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bottomNavBar = findViewById(R.id.nav_bar)
+        frameContainer = findViewById(R.id.frame_container)
+        var fragmentManager = supportFragmentManager
+        var fragmentTran = fragmentManager.beginTransaction()
+        frag = RecipeFragment()
+        fragmentTran.replace(R.id.frame_container, frag)
+        fragmentTran.commit()
+        bottomNavBar.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.recipes_menu -> {
+                    frag = RecipeFragment()
+                    fragmentTran = fragmentManager.beginTransaction()
+                    fragmentTran.replace(R.id.frame_container, frag).commit()
+                }
+                R.id.cart_menu -> {
+                    frag = CartFragment()
+                    fragmentTran = fragmentManager.beginTransaction()
+                    fragmentTran.replace(R.id.frame_container, frag).commit()
+                }
+                R.id.jokes_menu -> {
+                    frag = JokesFragment()
+                    fragmentTran = fragmentManager.beginTransaction()
+                    fragmentTran.replace(R.id.frame_container, frag).commit()
+                }
+            }
+            true
+        }
+    }
+}
